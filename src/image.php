fbe7e1fcff5fc018fa4ff19b8bede78457be7010
mvl22 <martin@lucas-smith.co.uk>
2004-10-01 12:00:00 +0000

    Version 0.9.0 (released 1/Oct/2004)
    * Initial beta release

---
+++ src/image.php
@@ -0,0 +1,236 @@
+<?php
+
+# Ensure the pureContent framework is loaded and clean server globals
+require_once ('pureContent.php');
+
+
+# Define a class containing image-related static methods
+class image {
+	
+	# Wrapper function to create a photo gallery
+	function gallery ($directory = './', $imageGenerationScript = 'image.html', $width = 200, $showAttributes = true, $displayImmediately = true)
+	{
+		# Load the directory support library
+		require_once ('directories.php');
+		
+		# Parse the specified directory so that it is always the directory from the server root
+		$directory = directories::parse ($directory);
+		
+		# Define the supported extensions
+		$supportedFileTypes = array (/*'gif', */'jpg', 'jpeg', 'png');
+		
+		# Read the directory, including only supported file types (i.e. extensions)
+		$files = directories::listFiles ($directory, $supportedFileTypes);
+		
+		# Show a message if there are no files in the directory and exit the function
+		if (count ($files) < 1) {
+			$html ='<p>There are no images to view in this location.</p>';
+			if ($displayImmediately) {echo $html;}
+			return $html;
+		}
+		
+		# Start the HTML block
+		$startHtml = "\n\t" . '<div class="gallery">';
+		if ($displayImmediately) {echo $startHtml;}
+		
+		# Loop through each file and construct the HTML
+		$compiledHtml = '';
+		foreach ($files as $file => $attributes) {
+			
+			# Define the link
+			$link = $file;
+			
+			# Define the HTML
+			$html = "\n" . '
+			<div class="image">
+				<a href="' . $link . '" target="_blank"><img src="' . $imageGenerationScript . '?' . $width . ',' . str_replace (' ', '%20', $directory) . str_replace (' ', '%20', $file) . '" width="' . $width . '" alt="[Click for full-size image; opens in a new window]" /></a>
+				' . (($showAttributes) ? '<p><strong>' . $file . '</strong> [' . round ($attributes['size'] / '1024', -1) . ' KB]</p>' : '')
+				. '<p>' . strftime ('%a %d/%b/%Y, %I:%M%p', $attributes['time']) . '</p>
+			</div>';
+			
+			# If necessary, display the HTML immediately
+			if ($displayImmediately) {echo $html;}
+			
+			# Also compile the HTML
+			$compiledHtml .= $html;
+		}
+		
+		# End the HTML
+		$endHtml = "\n\n\t" . '</div>' . "\n";
+		if ($displayImmediately) {echo $endHtml;}
+		
+		# Return the compiled HTML in case that is needed
+		return $startHtml . $compiledHtml . $endHtml;
+	}
+	
+	
+	# Function to resize an image; supported input and output formats are: jpg, png
+	function resize ($sourceFile, $outputFormat = 'jpg', $newWidth = '', $newHeight = '')
+	{
+		# Decode the $sourceFile to remove HTML entities
+		$sourceFile = str_replace ('//', '/', $_SERVER['DOCUMENT_ROOT'] . str_replace ('%20', ' ', $sourceFile));
+		
+		# Check that the file exists for security reasons
+		if (!file_exists ($sourceFile)) {echo '<p>Error: the selected file could not be found.</p>'; return false;}
+		
+		# Obtain the input format by taking the file extension, allowing for .jpeg and .jpg for JPG format
+		$inputFileExtension = substr ($sourceFile, -4);
+		if (substr ($sourceFile, -5) == '.jpeg') {$inputFileExtension = '.jpg';}
+		
+		# Obtain the source image
+		switch ($inputFileExtension) {
+				
+			/* # GIF format
+			case '.gif':
+				$sourceFile = ImageCreateFromGIF ($sourceFile);
+				break; */
+				
+			# JPG format
+			case '.jpg':
+				$sourceFile = ImageCreateFromJPEG ($sourceFile);
+				break;
+				
+			# PNG format
+			case '.png':
+				$sourceFile = ImageCreateFromPNG ($sourceFile);
+				break;
+				
+			# If an invalid format has been requested, return false
+			default:
+				 echo '<p>Error: an unsupported input format was requested.</p>';
+				 return false;
+		}
+		
+		# Obtain the height and width
+		$originalWidth = ImageSx ($sourceFile);
+		$originalHeight = ImageSy ($sourceFile);
+		
+		# Ensure that a valid width and height have been entered
+		if (!is_numeric ($newWidth) && !is_numeric ($newHeight)) {
+			$newWidth = $originalWidth;
+			$newHeight = $originalHeight;
+		}
+		
+		# Assign the width and height, proportionally if necessary
+		$newWidth = (is_numeric ($newWidth) ? $newWidth : (($newHeight / $originalHeight) * $originalWidth));
+		$newHeight = (is_numeric ($newHeight) ? $newHeight : (($newWidth / $originalWidth) * $originalHeight));
+		
+		# Create the resized image
+		$output = ImageCreateTrueColor ($newWidth, $newHeight);
+		ImageCopyResampled ($output, $sourceFile, 0, 0, 0, 0, $newWidth, $newHeight, $originalWidth, $originalHeight);
+		
+		# Send the image
+		switch ($outputFormat) {
+				
+			/* # GIF format
+			case 'gif':
+				header ("Content-Type: image/gif");
+				ImageGIF ($output);
+				break; */
+				
+			# JPG format
+			case 'jpg':
+			case 'jpeg':
+				header ("Content-Type: image/jpg");
+				ImageJPEG ($output);
+				break;
+				
+			# PNG format
+			case 'png':
+				header ("Content-Type: image/png");
+				ImagePNG ($output);
+				break;
+				
+			# If an invalid format has been requested, return false
+			default:
+				 echo '<p>Error: an unsupported output format was requested.</p>';
+				 return false;
+		}
+	}
+	
+	
+	# Function to display a gallery of files
+	function switchableGallery ()
+	{
+		# Get a listing of files in the current directory (this assumes the current page is called index.html)
+		require_once ('directories.php');
+		$rawFiles = directories::listFiles ($_SERVER['PHP_SELF']);
+		
+		# Loop through the list of files
+		foreach ($rawFiles as $file => $attributes) {
+			
+			# List only jpeg files by creating a new array of weeded files
+			if ($attributes['extension'] == 'jpg') {
+				$files[$file] = $attributes;
+			}
+		}
+		
+		# Sort the files alphabetically
+		asort ($files);
+		
+		# Start a variable to hold the HTML
+		$html = '';
+		
+		# Count the number of files and proceed only if there are any
+		$totalFiles = count ($files);
+		if ($totalFiles > 0) {
+			
+			# Begin the HTML list
+			$jumplist = "\n" . '<p class="jumplist">Go to page:';
+			
+			# Loop through each file
+			$i = 0;
+			foreach ($files as $file => $attributes) {
+				
+				# If the file is the first, store it in memory in case it is needed
+				if ($i == 0) {$firstFile[$file] = $attributes;}
+				
+				# Advance the counter
+				$i++;
+				
+				# Pick out the currently selected image, based on the query string (if any) and assign a CSS flag
+				#!# Somehow here need to add class="selected" to the first item when there is no query string or $i is 1?
+				$selected = '';
+				if ($attributes['name'] == $_SERVER['QUERY_STRING']) {
+					$showFile[$file] = $attributes;
+					$selected = ' class="selected"';
+				} else if ($i == 1) {
+					$selected = ' class="first"';
+				}
+				
+				# Add the file to the jumplist of files
+				$jumplist .= " <a href=\"?{$attributes['name']}\"$selected>{$attributes['name']}</a>";
+			}
+			
+			# End the HTML link list
+			$jumplist .= '</p>';
+			
+			# Add in the HTML link list
+			$html .= $jumplist;
+			
+			# If no query string was given, or the query string does not match any file, select the first file as the one to be shown
+			if (!isSet ($showFile)) {
+				$showFile = $firstFile;
+			}
+			
+			# Get the filename
+			foreach ($showFile as $name => $attributes) {
+				break;
+			}
+			
+			# Get the image size
+			list ($width, $height, $type, $attributes) = getimagesize ($_SERVER['DOCUMENT_ROOT'] . $_SERVER['PHP_SELF'] . $name);
+			
+			# Show the image
+			$html .= "\n\n<img src=\"$name\" $attributes alt=\"Page {$attributes['name']}\" />";
+			
+			# Add in the HTML link list again
+			$html .= $jumplist;
+		}
+		
+		# Return the HTML
+		return $html;
+	}
+}
+
+?>
